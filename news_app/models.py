f3a7375c7937d8a97729d8797e6d70ec1ff3c569
Julia Choi <julia.choi7@outlook.com>
2026-03-31 15:54:56 -0500

    Added docstrings to models.py

--- news_app/models.py
+++ news_app/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+"""Database models for News articles and Custom Users."""
+
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
